e976e3b8f5386c66fd86be43c4bf0c50b3ff65ba
Mateusz Zając <matzayonc@gmail.com>
2026-05-21 12:18:01 +0200

    handling github base url

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -7,7 +7,6 @@ import wasm from 'vite-plugin-wasm'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jbl/',
   plugins: [react(), tailwindcss(), nodePolyfills(), wasm()],
   define: {
     global: 'window',
